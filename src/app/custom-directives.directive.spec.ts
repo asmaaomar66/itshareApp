611b828f7fed24272cc154bc55754a0adf3c9025
AsmaaOmar <62079548+asmaaomar66@users.noreply.github.com>
2021-01-24 20:39:25 +0200

    Auto-generated commit

--- src/app/custom-directives.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CustomDirectivesDirective } from './custom-directives.directive';
-
-describe('CustomDirectivesDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CustomDirectivesDirective();
-    expect(directive).toBeTruthy();
-  });
-});
